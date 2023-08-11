50293e22bd3a4b2019aeb08f42494125440a5ec7
Andy Nguyen <139949316+andyn-network@users.noreply.github.com>
2023-08-11 11:21:32 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+// Import necessary dependencies and styles
+import { useState } from 'react';
 import quotes from "./assets/quotes.json";
 import { FaTwitter, FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import './App.css';
 
+// Define the structure of a quote using TypeScript interface
 interface Quote {
   quote: string;
   author: string;
 }
 
+// Function to get a random quote from the quotes array
 const getRandomQuote = (): Quote => {
   return quotes[Math.floor(Math.random() * quotes.length)];
 }
 
+// Function to generate a random RGB color
 const getRandomColor = (): string => {
   const red = Math.floor(Math.random() * 128);
   const green = Math.floor(Math.random() * 128);
@@ -20,45 +24,57 @@ const getRandomColor = (): string => {
   return `rgb(${red}, ${green}, ${blue})`;
 };
 
+// Define the CSS transition animation
 const transition = "all 1s";
 
 function App() {
-  const [quote, setQuote] = useState<Quote>(getRandomQuote());
-  const [randomColor, setRandomColor] = useState<string>(getRandomColor());
+  // State variables using useState hook
+  const [quote, setQuote] = useState<Quote>(getRandomQuote()); // Holds the current quote
+  const [randomColor, setRandomColor] = useState<string>(getRandomColor()); // Holds the background color
 
+  // Function to change the displayed quote and background color
   const changeQuote = () => {
     setQuote(getRandomQuote());
     setRandomColor(getRandomColor());
   }
 
   return (
-    <div className="background" style={{backgroundColor: randomColor, transition }}>
+    <div className="background" style={{ backgroundColor: randomColor, transition }}>
       <div id="quote-box">
-        <div className="quote-content" style={{color: randomColor}}>
+        <div className="quote-content" style={{ color: randomColor }}>
           <h2 id="text">
-          <FaQuoteLeft size="30" style={{ marginRight: "10px"}}/>
+            {/* FontAwesome icons for quotation marks */}
+            <FaQuoteLeft size="30" style={{ marginRight: "10px" }}/>
             {quote.quote}
-          <FaQuoteRight size="30" style={{ marginLeft: "10px"}}/>
-            </h2>
+            <FaQuoteRight size="30" style={{ marginLeft: "10px" }}/>
+          </h2>
           <h4 id="author">- {quote.author}</h4>
         </div>
         <div className="buttons">
-          <a 
-          href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}`}
-          id="tweet-quote"
-          style={{
-            backgroundColor: randomColor,
-            marginRight: "10px",
-            transition,
-          }}
+          {/* Tweet button */}
+          <a
+            href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}`}
+            id="tweet-quote"
+            style={{
+              backgroundColor: randomColor,
+              marginRight: "10px",
+              transition,
+            }}
           >
             <FaTwitter color="white" />
           </a>
-          <button id="new-quote" onClick={changeQuote} style={{backgroundColor: randomColor, transition }}>Change Quote</button>
+          {/* Change Quote button */}
+          <button
+            id="new-quote"
+            onClick={changeQuote}
+            style={{ backgroundColor: randomColor, transition }}
+          >
+            Change Quote
+          </button>
         </div>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
